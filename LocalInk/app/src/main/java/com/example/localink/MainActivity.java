7d53a72c2de2333e29b513dd4d77b0947cfe7e05
Maya Epps <mepps@fb.com>
2020-07-13 14:24:33 -0700

    Set up bottom navigation view and fragments
    - Created fragments for the home feed, wishlist, and profile screens
    - Implemented switching between each fragment when the respective icon is tapped
    - Implemented view binding in MainActivity

--- LocalInk/app/src/main/java/com/example/localink/MainActivity.java
+++ LocalInk/app/src/main/java/com/example/localink/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.localink;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.pm.PermissionInfoCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.localink.Fragments.ProfileFragment;
+import com.example.localink.Fragments.RecommendationsFragment;
+import com.example.localink.Fragments.WishlistFragment;
+import com.example.localink.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // view binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+        setContentView(view);
+
+        final Fragment recommendationsFragment = new RecommendationsFragment();
+        final Fragment wishlistFragment = new WishlistFragment();
+        final Fragment profileFragment = new ProfileFragment();
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        fragment = recommendationsFragment;
+                        break;
+                    case R.id.action_wishlist:
+                        fragment = wishlistFragment;
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        fragment = profileFragment;
+                        break;
+                }
+                // Switch out the frame layout with the specified fragment
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // Set default selection so when the app loads for the first time, it will have a fragment loaded
+        binding.bottomNavigation.setSelectedItemId(R.id.action_home);
     }
 }
