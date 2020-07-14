853a7df4eaeb406c910ca904aeb72bf5918a1d9d
Maya Epps <mepps@fb.com>
2020-07-14 12:46:12 -0700

    Added skeleton views for the login and registration screens
    - Used Material Design views to add fields to registration screens
    - Added login and password edit text views in the login screen

--- LocalInk/app/src/main/java/com/example/localink/BookDetailsActivity.java
+++ LocalInk/app/src/main/java/com/example/localink/BookDetailsActivity.java
@@ -2,13 +2,35 @@ package com.example.localink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.localink.Fragments.WishlistFragment;
+import com.example.localink.databinding.ActivityBookDetailsBinding;
+import com.example.localink.databinding.ActivityEditBookstoreProfileBinding;
+import com.example.localink.databinding.ActivityEditProfileBinding;
 
 public class BookDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_details);
+
+
+        // view binding
+        ActivityBookDetailsBinding binding = ActivityBookDetailsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+        setContentView(view);
+
+        binding.fabAddToWishlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Add book to wishlist
+                Intent i = new Intent(BookDetailsActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
