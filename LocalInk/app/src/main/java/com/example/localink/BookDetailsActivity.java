ed395dcfdba47e374b7deeab47d88402828b371f
Maya Epps <mepps@fb.com>
2020-07-22 13:52:52 -0700

    Open the store in Google Maps when the map fragment is tapped
    - Set an on click listener in the maps fragment
    - Open Google Maps using implicit intent when the framgent is tapped
    - Create a uri with the store name and address to search in google maps
    
    https://github.com/mayaepps/friendly-barnacle/issues/11

--- LocalInk/app/src/main/java/com/example/localink/BookDetailsActivity.java
+++ LocalInk/app/src/main/java/com/example/localink/BookDetailsActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
--- LocalInk/app/src/main/java/com/example/localink/Fragments/MapsFragment.java
+++ LocalInk/app/src/main/java/com/example/localink/Fragments/MapsFragment.java
@@ -1,5 +1,7 @@
 package com.example.localink.Fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -19,11 +21,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.ParseGeoPoint;
 import com.parse.ParseUser;
 
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 
 public class MapsFragment extends Fragment {
@@ -47,8 +50,32 @@ public class MapsFragment extends Fragment {
         public void onMapReady(GoogleMap googleMap) {
             stores = getStores();
             setMarkers(googleMap);
+
+            // If there is only one store on the map, let the user tap the map to see the store in the Google Maps app
+            if (stores.size() == 1) {
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(LatLng arg0) {
+                        android.util.Log.i("onMapClick", "Horray!");
+                        LocalInkUser store = new LocalInkUser(stores.get(0));
+                        goGoogleMaps(store);
+                    }
+                });
+            }
         }
     };
+
+    // Open google maps and search for the bookstore
+    private void goGoogleMaps(LocalInkUser store) {
+         // Create a Uri from an intent string. Use the result to create an Intent.
+        Uri mapsIntentUri = Uri.parse("https://www.google.com/maps/search/?api=1&query=" + Uri.encode(store.getName() + ", " + store.getLocation()));
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapsIntentUri);
+        // Make sure there is an app that can handle this intent
+        if (mapIntent.resolveActivity(getContext().getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+    }
 
     // Put a marker on the map at each of the stores
     private void setMarkers(GoogleMap map) {
@@ -85,8 +112,7 @@ public class MapsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
         }
